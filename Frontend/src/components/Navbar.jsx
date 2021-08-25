ff0895de9aa064c87e3b869541f5b8a637ca7e6b
Samridh Sharma <87688215+Samridh-Sharma@users.noreply.github.com>
2021-08-25 14:45:52 +0530

    Updated navbar with homepage

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,45 +1,106 @@
-import React from 'react'
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import {useHistory} from "react-router-dom";
 import Home from "./Home";
 import Categories from "./Categories";
 import AboutUs from "./AboutUs";
 import MyCart from "./MyCart";
-import SignIn from "./SignIn";
+import SignUp from "./SignUp";
 import Register from "./Register";
-export function Navbar2() {
-  let history=useHistory();
+import "./Navbar.css";
+export class Navbar2 extends Component {
+  render() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: "black" }}>
-          <a class="navbar-brand" onClick={()=>{history.push("/")}} to="">Photo Shopee</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <Router>
+        <nav
+          class="navbar navbar-expand-lg navbar-dark"
+          style={{ backgroundColor: "#EFEFEF" }}
+        >
+          <Link
+            class="navbar-brand"
+            // style={{ padding: "15px 48px 12px 28px", borderRadius: "20px" }}
+            to=""
+            style={{
+              fontFamily: "Open Sans, sans-serif",
+              fontSize: "30px",
+              color: "black",
+              paddingLeft: "30px",
+            }}
+          >
+            PHOTO WALLS
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav me-auto">
               <li class="nav-item active">
-                <a class="nav-link" onClick={()=>{history.push("/")}} >Home <span class="sr-only">(current)</span></a>
+                <Link class="nav-link" to="">
+                  <h4>Home</h4> <span class="sr-only">(current)</span>
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" onClick={()=>{history.push("/categories")}}>Categories</a>
+                <Link class="nav-link" to="/categories">
+                  <h4>Categories</h4>
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" onClick={()=>{history.push("/aboutUs")}}>About Us</a>
+                <Link class="nav-link" to="/aboutUs">
+                  <h4>About Us</h4>
+                </Link>
+              </li>
+              {/* <li class="nav-item">
+                <Link class="nav-link" to="/myCart">
+                  My Cart
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" onClick={()=>{history.push("/myCart")}}>My Cart</a>
+                <Link class="nav-link" to="/signUp">
+                  Signup
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" onClick={()=>{history.push("/signIn")}}>Signin</a>
+                <Link class="nav-link" to="/register">
+                  Register
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" onClick={()=>{history.push("/register")}}>Register</a>
-              </li>
+                {" "}
+                <a class="nav-link" style={{ marginLeft: "5px" }} href="#">
+                  <h5>My Cart</h5>
+                </a>
+              </li> */}
             </ul>
           </div>
+          <ul class="navbar-nav d-flex me-4">
+            <li class="nav-item ">
+              {" "}
+              <Link class="nav-link" href="#">
+                <h4>Login</h4>
+              </Link>
+            </li>
+          </ul>
         </nav>
-    )
-  
+        <div className="container mt-2">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/categories" component={Categories} />
+            <Route path="/aboutUs" component={AboutUs} />
+            <Route path="/myCart" component={MyCart} />
+            <Route path="/signUp" component={SignUp} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default Navbar2
+export default Navbar2;
